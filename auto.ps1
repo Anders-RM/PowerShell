e7b6de43a3cff2c341263bd4dad215829765d64e
anddersrm1808 <andersrm1808@gmail.com>
2023-02-03 21:21:11 +0100

    Test 03/02/2023  21:21

--- auto.ps1
+++ auto.ps1
@@ -1,8 +1,8 @@
 Invoke-WebRequest -Uri https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile "$PSScriptRoot/DesktopAppInstaller.msixbundle" -UseBasicParsing 
 Invoke-WebRequest -Uri https://kbhost.nl/wp-content/uploads/2022/04/Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx.zip -OutFile "$PSScriptRoot/VCLibs.Appx.zip" -UseBasicParsing 
-Expand-Archive C:\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx.zip C:\
-Add-AppxPackage -Path "C:\Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx"
-Add-AppPackage -Path "C:\Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Expand-Archive "$PSScriptRoot/VCLibs.Appx.zip" $PSScriptRoot 
+Add-AppxPackage -Path "$PSScriptRoot/Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx"
+Add-AppPackage -Path "$PSScriptRoot/DesktopAppInstaller.msixbundle"
 
 
 
