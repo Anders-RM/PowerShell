f89ecf15c5f9ca6ca2252ef4227cf2adad166bb9
anddersrm1808 <andersrm1808@gmail.com>
2023-02-03 21:08:30 +0100

    test 03/02/2023 31:08

--- auto.ps1
+++ auto.ps1
@@ -1,2 +1,3 @@
 Invoke-WebRequest -uri https://github.com/microsoft/winget-cli/releases/download/v1.4.10173/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle -OutFile "$PSScriptRoot/winget.msixbundle"
 add-appxpackage -Path "$PSScriptRoot/winget.msixbundle"
+Read-Host -Prompt "Press any key to continue..."
