84d52b875d08c3567f47800cde62f2c2f3e21564
anddersrm1808 <andersrm1808@gmail.com>
2023-02-03 21:24:30 +0100

    Test 03/02/2023 21:24

--- auto.ps1
+++ auto.ps1
@@ -4,9 +4,10 @@ Expand-Archive "$PSScriptRoot/VCLibs.Appx.zip" $PSScriptRoot
 Add-AppxPackage -Path "$PSScriptRoot/Microsoft.VCLibs.140.00.UWPDesktop_14.0.30704.0_x64__8wekyb3d8bbwe.Appx"
 Add-AppPackage -Path "$PSScriptRoot/DesktopAppInstaller.msixbundle"
 
-
-
-
+winget install --id=Mozilla.Firefox.ESR  -e
+winget install --id=Microsoft.VisualStudioCode  -e
+winget install --id=Git.Git  -e
+winget install --id=7zip.7zip  -e
 
 
 
